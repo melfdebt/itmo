6570c6bdd585d8a144beffbb19a37417a45863e6
melfdebt <159796564+melfdebt@users.noreply.github.com>
2026-09-17 18:15:41 +0300

    Add files via upload

--- основы программирования п1/bullet.gd
+++ основы программирования п1/bullet.gd
@@ -11,4 +11,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position+= speed_of_flight*Vector2.UP.rotated(rotation)*delta
+	position+= speed_of_flight*Vector2.UP.rotated(rotation + PI/2)*delta
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("collided")
+	self.queue_free()
